da18aa90be53697d3cd98dbd16b4284897e344f0
shimizu510 <49830209+shimizu510@users.noreply.github.com>
2026-03-14 21:56:09 +0900

    Update Astro configuration to Remove adapter-static from astro.configuse static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
-  adapter: staticAdapter(),
+  output: 'static',
   trailingSlash: 'always',
 });
