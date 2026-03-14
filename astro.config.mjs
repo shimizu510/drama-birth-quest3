d414d01f466620b13236066e9e0aef1657e0e593
shimizu510 <49830209+shimizu510@users.noreply.github.com>
2026-03-14 22:01:05 +0900

    Add static adapter to Astro coUse Astro static adapter in confignfiguration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
+  adapter: staticAdapter(),
   output: 'static',
   trailingSlash: 'always',
 });
